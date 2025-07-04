0c742e6de1543fade7eb6388cf443d14a327de63
sergey90qa <sergey.yaroslavtsev.dev@yandex.ru>
2025-07-04 14:57:52 +0700

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,45 +1,18 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect
 import pytest
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+def test_empty_courses_list(chromium_page_with_state):
+    chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+    title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(title).to_be_visible()
 
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(icon).to_be_visible()
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
+    empty_list_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_list_title).to_be_visible()
 
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        context.storage_state(path='context.json')
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='context.json')
-        page = context.new_page()
-
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-
-        title = page.get_by_test_id('courses-list-toolbar-title-text')
-        expect(title).to_be_visible()
-
-        icon = page.get_by_test_id('courses-list-empty-view-icon')
-        expect(icon).to_be_visible()
-
-        empty_list_title = page.get_by_test_id('courses-list-empty-view-title-text')
-        expect(empty_list_title).to_be_visible()
-
-        empty_description = page.get_by_test_id('courses-list-empty-view-description-text')
-        expect(empty_description).to_be_visible()
+    empty_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_description).to_be_visible()
