3aba6951f1b37688088bda7bd6c4dbff271bc9aa
sergey90qa <sergey.yaroslavtsev.dev@yandex.ru>
2025-07-10 16:05:14 +0700

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import expect
+from pages.create_course_page import CreateCoursePage
+from pages.courses_list_page import CoursesListPage
 import pytest
 
 @pytest.mark.courses
@@ -16,3 +18,23 @@ def test_empty_courses_list(chromium_page_with_state):
 
     empty_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
     expect(empty_description).to_be_visible()
+
+def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+    create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_create_course_form('','','','0','0')
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.upload_preview_image('./testdata/files/image.jpg')
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.fill_create_course_form('Playwright', '2 weeks', 'Playwright', '100', '10')
+    create_course_page.click_create_course_button()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_courses_card('Playwright', '100', '10', '2 weeks', 0)
+
