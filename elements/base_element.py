7e518795cbd0a42e23ad0260f14125c5f652d43e
sergey90qa <sergey.yaroslavtsev.dev@yandex.ru>
2025-07-17 16:20:27 +0700

    Практические применение PageFactory в автотестах

--- elements/base_element.py
+++ elements/base_element.py
@@ -6,18 +6,18 @@ class BaseElement:
         self.name = name
         self.locator =locator
 
-    def get_locator(self, **kwargs) -> Locator:
+    def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         locator = self.locator.format(**kwargs)
-        return self.page.get_by_test_id(locator)
+        return self.page.get_by_test_id(locator).nth(nth)
 
-    def click(self, **kwargs):
-        locator = self.get_locator(**kwargs)
-        locator.click()
+    def click(self, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth,**kwargs)
+        locator.nth(nth).click()
 
-    def check_visible(self, **kwargs):
-        locator = self.get_locator(**kwargs)
-        expect(locator).to_be_visible()
+    def check_visible(self, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth,**kwargs)
+        expect(locator.nth(nth)).to_be_visible()
 
-    def check_have_text(self, text: str, **kwargs):
-        locator = self.get_locator(**kwargs)
-        expect(locator).to_have_text(text)
+    def check_have_text(self, text: str, nth: int = 0,  **kwargs):
+        locator = self.get_locator(nth,**kwargs)
+        expect(locator.nth(nth)).to_have_text(text)
